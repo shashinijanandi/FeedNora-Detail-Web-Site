951b4eb32e6ddf2cad220d3859d9fbe5f1d01d51
It22902184 <127286216+It22902184@users.noreply.github.com>
2026-04-27 00:24:32 +0530

    update vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FeedNora-Detail-Web-Site/',
+  base: '/',
   server: {
     proxy: {
-      '/api': 'httSp://localhost:5000'
+      '/api': 'http://localhost:5000'
     }
   }
 })
